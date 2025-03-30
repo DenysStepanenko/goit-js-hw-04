e8b65714c1de3b39826de5c8d8e45d969d4cabcb
Denys Stepanenko <denys.workspace@gmail.com>
2025-03-30 14:57:44 -0600

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+'use strict';
+function makeArray(firstArray, secondArray, maxLength) {
+    let myArray = firstArray.concat(secondArray);
+    if (myArray.length > maxLength) {
+        return myArray.slice(0, maxLength);
+    } else {
+
+    } return myArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
