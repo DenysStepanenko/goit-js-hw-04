eec7c06300261293712d2d640cb1a6bb2cc02d3f
Denys Stepanenko <denys.workspace@gmail.com>
2025-03-30 15:13:13 -0600

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,26 @@
 'use strict';
-function filterArray(numbers, value) {
-    let overNumbers = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            overNumbers.push(numbers[i]);
-        }
-    } return overNumbers;
-}
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    }
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// Код для перевірки
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
