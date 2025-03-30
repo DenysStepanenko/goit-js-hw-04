26519336ae86c7b801dec87905cefcaccc33e697
Denys Stepanenko <denys.workspace@gmail.com>
2025-03-30 15:08:56 -0600

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,44 @@
 'use strict';
-function makeArray(firstArray, secondArray, maxLength) {
-    let myArray = firstArray.concat(secondArray);
-    if (myArray.length > maxLength) {
-        return myArray.slice(0, maxLength);
-    } else {
-
-    } return myArray;
+function calcAverageCalories(days) {
+    // Якщо масив порожній, повертаємо 0
+    if (days.length === 0) {
+        return 0;
+    }
+    
+    // Обчислюємо загальну кількість калорій за допомогою reduce
+    const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
+    
+    // Обчислюємо середнє, ділячи загальну суму на кількість днів
+    const averageCalories = totalCalories / days.length;
+    
+    return averageCalories;
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+// Код для перевірки
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
